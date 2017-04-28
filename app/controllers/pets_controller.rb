3cf16e0ae00150381d9ac4661b3d748d773dd631
Alvaro Quero <aquerorodriguez@gmail.com>
2017-04-28 21:49:00 +0200

    Now the pets can be added to db

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,16 @@ class PetsController < ApplicationController
   def create
     # render plain: params[:user].inspect
     @pet = Pet.new(entry_params)
+    @user = User.find(params[:id])
 
     if(@pet.save)
-      redirect_to home_url
+      redirect_to users_url(@user)
     else
       render 'new'
     end
+  end
+
+  private def entry_params
+    params.require(:pet).permit(:name, :gender, :age, :alive, :user_id, :pet_type_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index', as: 'home'
 
+  get '/users/:id/pets/new/', to: 'pets#new', as: 'new_pet'
+  post '/users/:id/pets/', to: 'pets#create', as: 'pets'
+
   resource :users
-  resource :pets
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
