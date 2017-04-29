88f55290b93e11fa1acc02680db8b35c1db5b7fa
Alvaro Quero <aquerorodriguez@gmail.com>
2017-04-29 12:16:57 +0200

    Validate email format and make it unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
-  # validates :email, uniqueness: true, on: :create
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   has_many :pets
 end
